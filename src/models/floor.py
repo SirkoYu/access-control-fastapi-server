dff8a23c64d9c0a0bfcdd972eb1e0ae3fadb7852
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 14:48:02 +0300

    feat(models): Add relationships for Building and Room in Floor model

--- src/models/floor.py
+++ src/models/floor.py
@@ -1,9 +1,18 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
 from .base import Base
 from .mixins import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from .building import Building
+    from .room import Room
+
+
 class Floor(Base, IntIdPkMixin):
     floor_number: Mapped[int]
     building_id: Mapped[int] = mapped_column(ForeignKey("buildings.id"))
+    building: Mapped["Building"] = relationship(back_populates="floors")
+    rooms: Mapped[list["Room"]] = relationship(back_populates="floor")
