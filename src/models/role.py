907e582b4cd3112f391ca3fd48853c73094e831f
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 14:47:08 +0300

    feat(models): Add relationship between AccessRule and User for Role model

--- src/models/role.py
+++ src/models/role.py
@@ -1,9 +1,19 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Text
 
 from .base import Base
 from .mixins import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from .access_rule import AccessRule
+    from .user import User
+
+
 class Role(Base, IntIdPkMixin):
     name: Mapped[str] = mapped_column(String(48), unique=True)
     description: Mapped[str] = mapped_column(Text)
+
+    access_rules: Mapped[list["AccessRule"]] = relationship(back_populates="role")
+    users: Mapped[list["User"]] = relationship(secondary="user_role_association", back_populates="roles")  # type: ignore
