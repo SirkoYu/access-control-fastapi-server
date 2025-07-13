e436111796551bdfb22fbb3c901e6d2d85a025c3
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 14:49:11 +0300

    feat(models): Add relationships for Room and User in CurrentPresence model

--- src/models/current_presence.py
+++ src/models/current_presence.py
@@ -1,11 +1,21 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
 from .base import Base
 from .mixins import IntIdPkMixin, TimestampMixin
+
+if TYPE_CHECKING:
+    from .room import Room
+    from .user import User
+
 
 class CurrentPresence(Base, IntIdPkMixin, TimestampMixin):
     __tablename__ = "current_presence" # type: ignore
 
     room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    room: Mapped["Room"] = relationship(back_populates="current_presence")
+    user: Mapped["User"] = relationship(back_populates="current_presence")
