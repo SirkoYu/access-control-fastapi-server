1476399a9cf4e6fef0cc233689b267db1835376f
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 12:39:41 +0300

    Create database settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, MySQLDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,6 +11,22 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: MySQLDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 50
+    pool_size: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env.template",".env",),
@@ -20,6 +36,7 @@ class Settings(BaseSettings):
     ) 
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
     
 settings = Settings()  # type: ignore
