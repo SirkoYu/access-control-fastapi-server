909721d197726740e8ad0c97460b738875cd4e9b
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 14:56:38 +0300

    feat(models): Add relationships for Room and User in AccessLog model

--- src/models/access_log.py
+++ src/models/access_log.py
@@ -1,17 +1,27 @@
 import enum
+from typing import TYPE_CHECKING
 
 from sqlalchemy import Enum, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 from .mixins import IntIdPkMixin, TimestampMixin
 
+if TYPE_CHECKING:
+    from .room import Room
+    from .user import User
+
+
 class Action(enum.StrEnum):
     enter = "Enter"
     exit = "Exit"
+
 
 class AccessLog(Base, IntIdPkMixin, TimestampMixin):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
     action: Mapped[Action] = mapped_column(Enum(Action, name="action_enum"))
     access_allowed: Mapped[bool] = mapped_column(default=True, server_default="1")
+
+    room: Mapped["Room"] = relationship(back_populates="access_logs")
+    user: Mapped["User"] = relationship(back_populates="access_logs")
