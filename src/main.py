aa251fc0231125030601ea6e4da8165ca16a1001
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 12:24:33 +0300

    Create heath check endpoint

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import PlainTextResponse
 
 from core.config import settings
 
 main_app = FastAPI()
+
+
+@main_app.get(
+    "/",
+    response_class=PlainTextResponse,
+    status_code=status.HTTP_200_OK,
+    summary="Health check",
+    description="""
+This endpoint is used to verify that the API server is up and running.
+It returns a simple plain text response `OK` if the service is healthy.
+Useful for uptime monitoring, load balancers, or readiness/liveness probes.
+""",
+    response_description="Plain text response confirming the server is healthy (`OK`).",
+)
+async def health_check():
+    return "OK"
 
 
 if __name__ == "__main__":
