c1826ebeea3573e1a87c5eb946ffd2d28fdda26e
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 14:44:08 +0300

    feat(models): Enhance User model with relationships to Role, AccessLog, and CurrentPresence

--- src/models/user.py
+++ src/models/user.py
@@ -1,8 +1,16 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 from .mixins import IntIdPkMixin
+
+if TYPE_CHECKING:
+    from .role import Role
+    from .access_log import AccessLog
+    from .current_presence import CurrentPresence
+
 
 class User(Base, IntIdPkMixin):
     first: Mapped[str] = mapped_column(String(32))
@@ -10,3 +18,7 @@ class User(Base, IntIdPkMixin):
     email: Mapped[str] = mapped_column(String(64), unique=True)
     password_hash: Mapped[str] = mapped_column(String(64))
     is_active: Mapped[bool] = mapped_column(default=True, server_default="1")
+
+    roles: Mapped[list["Role"]] = relationship(secondary="user_role_association", back_populates="users") # type: ignore
+    access_logs: Mapped[list["AccessLog"]] = relationship(back_populates="user")
+    current_presence: Mapped["CurrentPresence"] = relationship(back_populates="user")
