86224bc3fc563c611fbe5abac99fdaf27870af36
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 14:21:31 +0300

    Create roles table

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "Building",
     "Floor",
     "Room",
+    "Role"
 )
 
 from .base import Base
 from .building import Building
 from .floor import Floor
 from .room import Room
+from .role import Role
