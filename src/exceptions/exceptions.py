1ac814dc1463ecd479d95b9172e12c13e8ac024f
Yurii Sirko <urasir1999@gmail.com>
2025-07-14 01:37:40 +0300

    Create new exception for auth

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -1,6 +1,10 @@
 from fastapi import HTTPException, status
 
-class InvalidCredentialsError(HTTPException):
+
+class AuthError(HTTPException):
+    pass
+
+class InvalidCredentialsError(AuthError):
     def __init__(self) -> None:
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED, 
@@ -8,12 +12,28 @@ class InvalidCredentialsError(HTTPException):
             headers={"WWW-Authenticate": "Bearer"},
         )
 
+class IncorectLoginData(AuthError):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
 class UserError(HTTPException):
     pass
+
 
 class UserNotFoundError(UserError):
     def __init__(self) -> None:
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND, 
             detail=f"User not found",
+        )
+
+class InactiveUserError(UserError):
+    def __init__(self, user_id: int| None = None) -> None:
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User is inactive" if user_id is None else f"User with {user_id} is inactive"
         )
