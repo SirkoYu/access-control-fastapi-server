d3936fcafb46dc9bd02b9c8deb3879bb8bb7e639
Yurii Sirko <urasir1999@gmail.com>
2025-07-13 15:15:53 +0300

    feat(schemas): Add AccessLog schemas

---
+++ src/schemas/access_log.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from ..models.access_log import Action
+from .general_schemas import User, Room
+
+class AccessLogBase(BaseModel):
+    user_id: int
+    room_id: int
+    action: Action
+    access_allowed: bool
+
+class AccessLogCreate(AccessLogBase):
+    pass
+
+class AccessLogUpdate(AccessLogBase):
+    pass
+
+class AccessLogUpdatePartical(BaseModel):
+    user_id: int|None = None
+    room_id: int|None = None
+    action: Action|None = None
+    access_allowed: bool|None = None
+
+class AccessLogOut(AccessLogBase):
+    id: int
+    timestamp: datetime
+
+class AccessLogWithUser(AccessLogOut):
+    user: User
+
+class AccessLogWithRoom(AccessLogOut):
+    room: Room
