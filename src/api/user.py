7af7a616dcf9ac77a812dead1c00fc12a41180d4
Yurii Sirko <urasir1999@gmail.com>
2025-07-14 03:46:34 +0300

    refactor(api): Simplify imports for user

--- src/api/user.py
+++ src/api/user.py
@@ -8,38 +8,30 @@ from crud import user as user_crud
 from exceptions.exceptions import UserNotFoundError
 from database.core import DBSession
 from models import User
-from schemas.user import (
-    UserCreate,
-    UserUpdate,
-    UserUpdatePatrical,
-    UserOut,
-    UserWithRoles,
-    UserWithCurrentPresence,
-    UserWithAccessLogs
-)
-from .dependepcies import get_user_by_id, IDField
+import schemas.user as schemas
+from .dependencies import get_user_by_id, IDField
 
 router = APIRouter(prefix="/user", tags=["Users"])
 
-@router.post("/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=schemas.UserOut, status_code=status.HTTP_201_CREATED)
 async def create_user(    
-    user_in: UserCreate,
+    user_in: schemas.UserCreate,
     session: DBSession,
 ):
     return await user_crud.create_user(user_in=user_in, session=session)
 
-@router.put("/{user_id}", response_model=UserOut)
+@router.put("/{user_id}", response_model=schemas.UserOut)
 async def update_user(
     session: DBSession,
-    user_in: UserUpdate,
+    user_in: schemas.UserUpdate,
     user: User = Depends(get_user_by_id),
 ):
     return await user_crud.update_user(session, user_in, user)
     
-@router.patch("/{user_id}", response_model=UserOut)
+@router.patch("/{user_id}", response_model=schemas.UserOut)
 async def update_user_partical(
     session: DBSession,
-    user_in: UserUpdatePatrical,
+    user_in: schemas.UserUpdatePatrical,
     user: User = Depends(get_user_by_id),
 ):
     return await user_crud.update_user(session, user_in, user, partical=True)
@@ -51,7 +43,7 @@ async def delete_user(
 ):
     await user_crud.delete_user(session, user)
 
-@router.get("/", response_model=list[UserOut])
+@router.get("/", response_model=list[schemas.UserOut])
 async def get_users(
     session: DBSession,
     offset: Annotated[int, Ge(0)] = 0,
@@ -60,7 +52,7 @@ async def get_users(
     users = await user_crud.get_users(session, offset, limit)
     return list(users)
 
-@router.get("/user-with-roles/{user_id}", response_model=UserWithRoles)
+@router.get("/user-with-roles/{user_id}", response_model=schemas.UserWithRoles)
 async def get_user_with_roles(
     session: DBSession,
     user_id: IDField
@@ -70,7 +62,7 @@ async def get_user_with_roles(
         raise UserNotFoundError
     return user
 
-@router.get("/user-with-current-presence/{user_id}", response_model=UserWithCurrentPresence)
+@router.get("/user-with-current-presence/{user_id}", response_model=schemas.UserWithCurrentPresence)
 async def get_user_with_current_presence(
     session: DBSession,
     user_id: IDField
@@ -80,7 +72,7 @@ async def get_user_with_current_presence(
         raise UserNotFoundError
     return user
 
-@router.get("/user-with-access_logs/{user_id}", response_model=UserWithAccessLogs)
+@router.get("/user-with-access_logs/{user_id}", response_model=schemas.UserWithAccessLogs)
 async def get_user_with_access_logs(
     session: DBSession,
     user_id: IDField
@@ -90,7 +82,7 @@ async def get_user_with_access_logs(
         raise UserNotFoundError
     return user
 
-@router.get("/users-with-roles", response_model=list[UserWithRoles])
+@router.get("/users-with-roles", response_model=list[schemas.UserWithRoles])
 async def get_users_with_roles(
     session: DBSession,
     offset: Annotated[int, Ge(0)] = 0,
@@ -99,7 +91,7 @@ async def get_users_with_roles(
     users = await user_crud.get_users_with_roles(session, offset, limit)
     return users
 
-@router.get("/users-with-current-presence", response_model=list[UserWithCurrentPresence])
+@router.get("/users-with-current-presence", response_model=list[schemas.UserWithCurrentPresence])
 async def get_users_with_current_presence(
     session: DBSession,
     offset: Annotated[int, Ge(0)] = 0,
@@ -108,7 +100,7 @@ async def get_users_with_current_presence(
     users = await user_crud.get_users_with_current_presence(session, offset, limit)
     return users
 
-@router.get("/users-with-access_logs", response_model=list[UserWithAccessLogs])
+@router.get("/users-with-access_logs", response_model=list[schemas.UserWithAccessLogs])
 async def get_users_with_access_logs(
     session: DBSession,
     offset: Annotated[int, Ge(0)] = 0,
@@ -117,7 +109,7 @@ async def get_users_with_access_logs(
     users = await user_crud.get_users_with_access_logs(session, offset, limit)
     return users
 
-@router.get("/{user_id}", response_model=UserOut)
+@router.get("/{user_id}", response_model=schemas.UserOut)
 async def get_user_with_id(
     user: User = Depends(get_user_by_id)
 ):
