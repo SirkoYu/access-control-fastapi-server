b9d2e2501895901aafcef8e9663b31b491afe4fc
Yurii Sirko <urasir1999@gmail.com>
2025-07-15 03:06:33 +0300

    Create general exceptions

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -1,46 +1,31 @@
-from fastapi import HTTPException, status
+from fastapi import status
+
+class AppException(Exception):
+    def __init__(
+        self,        
+        detail: str,
+        message: str|None = None,
+        status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
+        original_exc: Exception|None = None,
+        log_error: bool = True,
+    ):
+        self.message = (message or detail) if original_exc is None else str(original_exc)
+        self.detail = detail
+        self.status_code = status_code
+        self.original_exc = original_exc
+        self.log_error = log_error
+
+        super().__init__(message)
+
+        if original_exc:
+            self.__cause__ = original_exc
 
 
-class NotFoundException(HTTPException):
-    def __init__(self, entity: str = "Entity", entity_id: int|None = None):
+class NotFoundException(AppException):
+    def __init__(self, entity: str = "", entity_id: int|None = None):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"{entity} not found" if entity_id is None else f"{entity} with id={entity_id} not found"
+            detail=f"{entity} not found" if entity_id is None else f"{entity} with id={entity_id} not found",
+            log_error=False,
         )
 
-class AuthError(HTTPException):
-    pass
-
-class InvalidCredentialsError(AuthError):
-    def __init__(self) -> None:
-        super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
-            detail="Invalid authentication credentials", 
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-class IncorectLoginData(AuthError):
-    def __init__(self) -> None:
-        super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-class UserError(HTTPException):
-    pass
-
-
-class UserNotFoundError(UserError):
-    def __init__(self, user_id: int|None = None) -> None:
-        super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail="User not found" if user_id is None else f"User with {user_id!r} not found",
-        )
-
-class InactiveUserError(UserError):
-    def __init__(self, user_id: int| None = None) -> None:
-        super().__init__(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="User is inactive" if user_id is None else f"User with {user_id} is inactive"
-        )
