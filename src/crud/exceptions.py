d3afe151493121f648aef00500a8a157e8bd66ad
Yurii Sirko <urasir1999@gmail.com>
2025-07-15 13:24:21 +0300

    Add FloorAlreadyExistsException

--- src/crud/exceptions.py
+++ src/crud/exceptions.py
@@ -50,15 +50,18 @@ class AlreadyExistsException(CrudException):
     """Raised when entity already exists (unique constraint violation)."""
     def __init__(
         self,
-        model_name: str,
+        model_name: str,        
+        detail: str|None = None,
         field_name: str|None = None,
         field_value: Any = None,
         status_code: int = status.HTTP_409_CONFLICT,
         log_error: bool = False
     ):
+        if detail is None:
+            detail = f"{model_name} with {field_name}={field_value} already exists" if field_name is not None else f"{model_name} with this fields already exists"
         super().__init__(
             model_name=model_name,
-            detail=f"{model_name} with {field_name}={field_value} already exists" if field_name is not None else f"{model_name} with this fields already exists",
+            detail=detail,
             status_code=status_code,
             log_error=log_error
         )
@@ -132,4 +135,13 @@ class RoleAlreadyExistsException(AlreadyExistsException):
             field_name="name",
             field_value=name,
             status_code=409,
+        )
+
+class FloorAlreadyExistsException(AlreadyExistsException):
+    """Raised when floor with same number in building already exists."""
+    def __init__(self, floor_number: int, building_id: int):
+        super().__init__(
+            model_name="Floor",
+            detail=f"Floor with number {floor_number} in building {building_id} already exists",
+            status_code=409
         )
